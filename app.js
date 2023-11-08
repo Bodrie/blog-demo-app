69c449246520424ac15fcf42edbd26cbeb6081ec
Dobromir Kirov <kirov0407@gmail.com>
2023-11-08 18:18:44 +0200

    addes express routes for the other pages

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ app.listen(3000);
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.use("/", (req, res) => {
+  res.status(404).render("404");
+});
